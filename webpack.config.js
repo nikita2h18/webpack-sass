93499d3fb27e2c8d7156607b260eec4c4f1f8d6e
Nikita Shaban <mikita.shaban@itechart-group.com>
2021-05-12 15:39:04 +0300

    add postcss

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,23 @@
 const path = require('path');
 const miniCss = require('mini-css-extract-plugin');
+
 module.exports = {
   entry: './src/app.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
-    rules: [{
-      test:/\.(s*)css$/,
-      use: [
-        miniCss.loader,
-        'css-loader',
-        'sass-loader',
-      ]
-    }]
+    rules: [
+      {
+        test: /\.(scss|css)$/,
+        use: [miniCss.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
+      },
+    ],
   },
   plugins: [
     new miniCss({
       filename: 'style.css',
     }),
-  ]
+  ],
 };
