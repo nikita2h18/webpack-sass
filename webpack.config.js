3e063648ba62f0e83eacec9cdc42c56ac1bee4d1
Nikita Shaban <mikita.shaban@itechart-group.com>
2021-05-12 11:40:15 +0300

    test hook

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,24 @@
-const path = require("path");
-const miniCss = require("mini-css-extract-plugin");
+const path = require('path');
+const miniCss = require('mini-css-extract-plugin');
 module.exports = {
-  entry: "./src/app.js",
+  entry: './src/app.js',
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
   },
   module: {
-    rules: [
-      {
-        test: /\.(s*)css$/,
-        use: [miniCss.loader, "css-loader", "sass-loader"],
-      },
-    ],
+    rules: [{
+      test:/\.(s*)css$/,
+      use: [
+        miniCss.loader,
+        'css-loader',
+        'sass-loader',
+      ]
+    }]
   },
   plugins: [
     new miniCss({
-      filename: "style.css",
+      filename: 'style.css',
     }),
-  ],
+  ]
 };
